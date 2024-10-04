90e5bab102d19806099dd1dd6525d55ac2a02650
Saif <saifisami2005@gmail.com>
2024-10-04 19:08:43 -0400

    Implemented string reversal recursive function.

--- recursion.js
+++ recursion.js
@@ -41,8 +41,9 @@ function findIndex(arr, val, i = 0) {
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {
-
+function revString(str, newStr = "", i = 0) {
+  if(i === str.length) return newStr;
+  return revString(str, newStr + str[(str.length - 1) - i], i + 1);
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
